48405fa09471ba4f61cb61bd91f6b5ed06b4d3c7
buxxmoney <87129838+buxxmoney@users.noreply.github.com>
2023-04-23 18:46:34 -0600

    tried to fix the jpg issue

--- mysite/lesimulatte/models.py
+++ mysite/lesimulatte/models.py
@@ -8,7 +8,7 @@ class Clothing(models.Model):
 
     name = models.CharField(max_length=50, default=" ")
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    image = models.ImageField(upload_to='Images', default='images/none/noimg.jpg')
+    image = models.ImageField(upload_to='media/Images/', default='images/none/noimg.jpg')
     desc = models.CharField(max_length=500, default="")
 
 
